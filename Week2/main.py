30710f6ae53e66ed2c4486b0baeb7389f08e8d9b
Melanie Toh <melaniex@usc.edu>
2022-05-28 10:04:37 +0800

    Fixed bug

--- Week2/main.py
+++ Week2/main.py
@@ -41,22 +41,21 @@ driver.find_element(By.CLASS_NAME, '_9AhH0').click()
 time.sleep(2)
 
 # Liking the photo
-driver.find_element(By.XPATH, '/html/body/div[6]/div[2]/div/article/div/div[2]/div/div/div[2]/section[1]/span[1]/button').click()
+# driver.find_element(By.XPATH, '/html/body/div[6]/div[2]/div/article/div/div[2]/div/div/div[2]/section[1]/span[1]/button').click()
 
 # For loop to like every single post on the profile
 for loop in range(1, post_num+1):
     #  First iteration
     if loop == 1:
         action = ActionChains(driver)
-        # action.double_click(driver.find_element(By.XPATH, '/html/body/div[6]/div[2]/div/article/div/div[1]/div/div[1]/div[2]/div/div/div/button'))
         action.double_click(driver.find_element(By.XPATH, '/html/body/div[6]/div[3]/div/article/div/div[2]/div/div/div[2]/section[1]/span[1]/button'))
         time.sleep(1)
         # Clicking the next post button (right)
-        driver.find_element(By.XPATH, '/html/body/div[6]/div[1]/div/div/div/button').click()
+        driver.find_element(By.XPATH, '/html/body/div[6]/div[2]/div/div/button').click()
         time.sleep(2)
     else:
         action = ActionChains(driver)
         action.double_click(driver.find_element(By.XPATH, '/html/body/div[6]/div[3]/div/article/div/div[2]/div/div/div[2]/section[1]/span[1]/button'))
         time.sleep(1)
-        driver.find_element(By.XPATH, '/html/body/div[6]/div[1]/div/div/div[2]/button').click()
+        driver.find_element(By.XPATH, '/html/body/div[6]/div[2]/div/div[2]/button').click()
         time.sleep(2)
